b6fa3075d807b93931b4a82f091629649355dc57
LuisPaucarRodrigo <luis.paucar.ro@gmail.com>
2024-05-20 17:20:55 -0500

    bug fix

--- app/src/main/java/com/hybrid/projectarea/model/ImageOverlay.kt
+++ app/src/main/java/com/hybrid/projectarea/model/ImageOverlay.kt
@@ -8,11 +8,13 @@ import android.graphics.Color
 object ImageOverlay {
     private val paint = Paint().apply {
         color = Color.WHITE
-        textSize = 50f
     }
 
     fun overlayTextOnImage(imageBitmap: Bitmap,logoBitmap: Bitmap, dateTime: String, latitude: Double, longitude: Double): Bitmap {
         val canvas = Canvas(imageBitmap)
+
+        val textSize = (canvas.height * 0.025f)
+        paint.textSize = textSize
 
         val datetime = dateTime
         val gps = "Lat: $latitude, Lon: $longitude "
@@ -20,10 +22,10 @@ object ImageOverlay {
         val xdate = (canvas.width - paint.measureText(datetime))
         val xgps = (canvas.width - paint.measureText(gps))
         val yimage = 20f
-        val ydate = canvas.height - 30f
+        val ydate = canvas.height - textSize - 10f
         val ygps = canvas.height - 10f
 
-        canvas.drawBitmap(logoBitmap, ximage, yimage, paint)
+        canvas.drawBitmap(logoBitmap, ximage, yimage, null)
         canvas.drawText(datetime, xdate, ydate, paint)
         canvas.drawText(gps, xgps, ygps, paint)
 
